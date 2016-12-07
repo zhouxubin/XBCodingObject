9c56af1c8e4bacd6acdd31fbf87f323829ee6d95
周旭斌 <18201432664@163.com>
2016-12-07 10:15:52 +0800

    修复BUG

--- coding协议/codingObject/XBCodingObject.m
+++ coding协议/codingObject/XBCodingObject.m
@@ -16,15 +16,13 @@
     unsigned int count = 0;
     
     Ivar *propertyArr = class_copyIvarList([self class], &count);
-    objc_property_t *propertyArray = class_copyPropertyList([self class], &count);
     for (int i = 0; i < count; i++) {
         Ivar property = propertyArr[i];
         const char *propertyNameC = ivar_getName(property);
         NSString *propertyNameOC = [[NSString alloc] initWithCString:propertyNameC encoding:NSUTF8StringEncoding];
         [properties addObject:propertyNameOC];
     }
-    free(propertyArray);
-    
+    free(propertyArr);
     
     return properties;
 }
